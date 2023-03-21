28453d2e742b6dedc71e49f3bc8be6b7760b483e
0dany <122172585+0dany@users.noreply.github.com>
2023-03-21 22:11:58 +0100

    Delete Source Code directory

--- Source Code/W11/ProgramData-OwlOS/Utility/Additional/new_context_menu.bat
+++
@@ -1,4 +0,0 @@
-Reg.exe delete "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f >nul 2>&1
-Reg.exe delete "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f >nul 2>&1
-taskkill /f /im explorer.exe >nul 2>&1 
-start explorer.exe >nul 2>&1
--- Source Code/W11/ProgramData-OwlOS/Utility/Additional/old_context_menu.bat
+++
@@ -1,4 +0,0 @@
-Reg.exe add "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /ve /t REG_SZ /d "" /f >nul 2>&1
-Reg.exe add "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /t REG_SZ /d "" /f >nul 2>&1
-taskkill /f /im explorer.exe >nul 2>&1 
-start explorer.exe >nul 2>&1
