3cd10c9b241542fafba04ce1dd523341ea246fcb
0dany <122172585+0dany@users.noreply.github.com>
2023-04-22 22:50:27 +0200

    Delete Source Code directory

--- Source Code/W10/ProgramData-OwlOS/Additional/old_context_menu.bat
+++
@@ -1,2 +0,0 @@
-Reg.exe add "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /ve /t REG_SZ /d "" /f
-Reg.exe add "HKCU\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /t REG_SZ /d "" /f
